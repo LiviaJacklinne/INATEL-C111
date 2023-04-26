929194aee63e7bc60d21eb9cc0a7352a422c6a09
Livia Jacklinne <99216080+LiviaJacklinne@users.noreply.github.com>
2023-04-26 19:57:46 -0300

    Execicio 2

--- Aula/02-Aula Numpy/main.py
+++ Aula/02-Aula Numpy/main.py
@@ -6,11 +6,11 @@ import random
 # vect = np.array([1, 2, 3])
 
 ''' CRIANDO UM NUMPY ARRAY 2 DIMENSSÕES (MATRIZ) '''
-# mtz = np.array([
-#     [1,2,3,4],
-#     [5,6,7,8],
-#     [9,10,11,12]
-# ])
+mtz = np.array([
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12]
+])
 
 # # SLICING 
 # print(mtz[:2,2:])
@@ -36,6 +36,28 @@ import random
 # print(vect)
 
 ''' CONCATENAÇÃO DE NYMPY ARRAY '''
-vect_concat1 = np.array([1,2,3,4])
-vect_concat2 = np.array([5,6,7,8])
-print(vect_concat1,vect_concat2)
+# vect_concat1 = np.array([1,2,3,4])
+# vect_concat2 = np.array([5,6,7,8])
+# print(vect_concat1,vect_concat2)
+
+'''---------------------------------------------------------------------'''
+''' AULA PARTE 2'''
+
+# Gerar numeros aleatórios "iguais" (plantar uma semente - seed)
+np.random.seed(5)
+
+# inicio, fim, quantidade de numeros
+vect = np.random.randint(1,10,10)
+print(vect)
+print(np.unique(vect, return_counts= True))
+
+# # soma de todas as colunas
+# print(mtz.sum(axis=0))
+# # soma de todas as linhas
+# # se colocarmos um indice retorna a soma somente daquela coluna
+# print(mtz.sum(axis=1)[0])
+
+
+# ''' Operação de broadcast, é fazer a operação de um array por escalar   '''
+# print(mtz/10)
+
---
+++ Exercicio/CAP4/Exercicio_PAG_19/main.py
@@ -0,0 +1,45 @@
+import numpy as np
+import random
+
+''' QUESTÃO 1 '''
+# Semente
+# np.random.seed(5)
+# vect = (np.random.randn(10))*100
+# vect_inteiro = vect.astype(int)
+# print(vect)
+# print(vect_inteiro)
+
+''' QUESTÃO 2 '''
+np.random.seed(10)
+mtz = np.random.randint(1,51,16).reshape(4,4)
+print('Matriz\n', mtz)
+print("-------------------")
+
+''' QUESTÃO 3 '''
+media_coluna = (mtz.sum(axis=0))
+media_linha = (mtz.sum(axis=1))
+
+
+
+print(f'Media das colunas {media_coluna}')
+print(f'Maior média {media_coluna.max()}')
+print("-------------------")
+
+print(f'Media das linhas {media_linha}')
+print(f'Maior média {media_coluna.max()}')
+
+''' QUESTÃO 4 '''
+# A função unique, retorna o array ordenado e a reptição dos numeros
+vect_ordenado, repeticoes = np.unique(mtz, return_counts=True)
+
+print(vect_ordenado) # Array ordenado
+print(repeticoes) # Itens que se repetem, indice relacionado ao Array
+
+i=0 # Variavel contadora auxiliar para acessar o indice
+for cont in repeticoes:
+    # print(i)
+    if cont == 2: # Estamos comparando a 2, pq são 2 valores que se repetem 2x
+        # Se o valor for igual a 2, acessa o vetor na posição i
+        # e pega o valor que ta lá
+        print(vect_ordenado[i])
+    i+=1
